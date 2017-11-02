75c8d63b6e07bcf1fedca885550f659aebe66577
teddyrendahl <trendahl@slac.stanford.edu>
2017-11-02 16:45:16 -0700

    API: Panel now only allows a single column of signals

--- typhon/panel.py
+++ typhon/panel.py
@@ -6,14 +6,15 @@ import logging
 ############
 # External #
 ############
-from pydm.PyQt.QtGui import QHBoxLayout, QFont, QLabel, QWidget, QGridLayout
-from pydm.widgets.label import PyDMLabel
+from pydm.PyQt.QtCore import QSize
+from pydm.PyQt.QtGui  import QHBoxLayout, QFont, QLabel, QWidget, QGridLayout
 from pydm.widgets.line_edit import PyDMLineEdit
 
 #############
 #  Package  #
 #############
 from .utils import channel_name
+from .widgets import TyphonLabel
 
 logger = logging.getLogger(__name__)
 
@@ -26,35 +27,19 @@ class Panel(QWidget):
     signals : OrderedDict, optional
         Signals to include in the panel
 
-    max_cols : int, optional
-        Number of columns of information to display
-
     parent : QWidget, optional
         Parent of panel
     """
-    def __init__(self, signals=None, max_cols=8, parent=None):
+    def __init__(self, signals=None, parent=None):
         super().__init__(parent=parent)
-        self.max_cols = max_cols
         self.signals  = dict()
-        self.layout   = QGridLayout(self)
+        #Set QGridLayout to widget
+        self._layout   = QGridLayout()
+        self.setLayout(self._layout)
         #Add supplied signals
         if signals:
             for name, sig in signals.items():
                 self.add_signal(sig, name)
-
-    @property
-    def current_column(self):
-        """
-        Current row of panel to add widgets
-        """
-        return 2*(len(self.signals)%self.max_cols)
-
-    @property
-    def current_row(self):
-        """
-        Current column of panels to add widgets
-        """
-        return len(self.signals) // self.max_cols
 
     def add_signal(self, signal, name):
         """
@@ -66,7 +51,7 @@ class Panel(QWidget):
         name : str
             Name of signal to display
         """
-        logger.info("Adding signal %r with label %s", signal, name)
+        logger.debug("Adding signal %s", name)
         #Create label
         label = QLabel(self)
         label.setText(name)
@@ -74,18 +59,18 @@ class Panel(QWidget):
         label_font.setBold(True)
         label.setFont(label_font)
         #Create signal display
-        val_display = QHBoxLayout(self)
+        val_display = QHBoxLayout()
         #Add readback
-        val_display.addWidget(PyDMLabel(init_channel=channel_name(signal._read_pv),
-                                        parent=self))
+        val_display.addWidget(TyphonLabel(init_channel=channel_name(signal._read_pv),
+                                          parent=self))
         #Add write
         if hasattr(signal, '_write_pv'):
-            logger.debug("Adding PyDMLineEdit for %s", name)
+            logger.debug("Adding LineEdit for %s", name)
             val_display.addWidget(PyDMLineEdit(init_channel=channel_name(signal._write_pv),
                                                parent=self))
         #Add displays to panel
-        self.layout.addWidget(label, self.current_row, self.current_column)
-        self.layout.addLayout(val_display, self.current_row, self.current_column+1)
+        self._layout.addWidget(label, len(self.signals), 0)
+        self._layout.addLayout(val_display, len(self.signals), 1)
 
         #Store signal
         self.signals[name] = signal
