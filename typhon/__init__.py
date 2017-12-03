a1efdd9d38af7f81046c2e0cb118cc2ddc1d951f
teddyrendahl <trendahl@slac.stanford.edu>
2017-12-02 16:35:45 -0800

    MAINT: Explicit import of high level classes

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -1,4 +1,5 @@
-
+from .display import TyphonDisplay, DeviceDisplay
+from .widgets import ComponentButton
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
