a0c6c7aed2818c9014e97e35644548583ab51ca1
teddyrendahl <trendahl@slac.stanford.edu>
2018-05-30 13:04:14 -0700

    MAINT: Top level import of load_stylesheet

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ['TyphonDisplay', 'DeviceDisplay', 'ComponentButton']
+__all__ = ['TyphonDisplay', 'DeviceDisplay', 'ComponentButton',
+           'load_stylesheet']
 from .display import TyphonDisplay, DeviceDisplay
 from .widgets import ComponentButton
+from .utils import load_stylesheet
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
