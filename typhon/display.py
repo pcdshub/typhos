774c0a677a25823606a0db31f14d184c6766e2ba
teddyrendahl <trendahl@slac.stanford.edu>
2018-06-05 11:42:17 -0700

    ENH: Add PyDMLogDisplay for Device.log as a tool

--- typhon/display.py
+++ typhon/display.py
@@ -12,6 +12,7 @@ from pydm.PyQt import uic
 from pydm.PyQt.QtCore import pyqtSlot, Qt, QModelIndex
 from pydm.PyQt.QtGui import QWidget, QVBoxLayout
 from pydm.widgets.drawing import PyDMDrawingImage
+from pydm.widgets.logdisplay import PyDMLogDisplay
 
 ###########
 # Package #
@@ -351,3 +352,6 @@ class DeviceDisplay(TyphonDisplay):
         methods = methods or list()
         for method in methods:
                 self.method_panel.add_method(method)
+        # Add a LogWidget to our toolset
+        self.add_tool('Device Log', PyDMLogDisplay(logname=device.log.name,
+                                                   level=logging.INFO))
