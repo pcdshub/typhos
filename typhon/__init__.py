790e6fbdf25cc86524a9f2e7137e08bc1838f2a5
teddyrendahl <trendahl@slac.stanford.edu>
2018-06-07 11:12:36 -0700

    FIX: Import Typhon plugins in top level

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ['TyphonDisplay', 'DeviceDisplay', 'use_stylesheet']
+__all__ = ['TyphonDisplay', 'DeviceDisplay', 'use_stylesheet',
+           'register_signal']
 from .display import TyphonDisplay, DeviceDisplay
 from .utils import use_stylesheet
+from .plugins import register_signal
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
