da8b21c7a479bff536a95eba4522b78fbba3976e
teddyrendahl <trendahl@slac.stanford.edu>
2018-06-06 23:20:44 -0700

    TST: Call connection as PyDMApplication would

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -16,8 +16,7 @@ def test_signal_connection(qapp):
     register_signal(sig)
     widget = WritableWidget()
     listener = widget.channels()[0]
-    sig_conn = SignalConnection("sig://my_signal",
-                                "my_signal")
+    sig_conn = SignalConnection(listener, 'my_signal')
     sig_conn.add_listener(listener)
     # Check that our widget receives the initial value
     qapp.processEvents()
@@ -26,11 +25,14 @@ def test_signal_connection(qapp):
     assert widget.value == 1
     # Check that we can push values back to the signal which in turn causes the
     # internal value at the widget to update
-    widget.send_value_signal.emit(2)
+    widget.send_value_signal[int].emit(2)
     qapp.processEvents()
     qapp.processEvents()  # Must be called twice. Multiple rounds of signals
     assert sig.get() == 2
     assert widget.value == 2
+    # Try changing types
+    qapp.processEvents()
+    qapp.processEvents()  # Must be called twice. Multiple rounds of signals
     sig_conn.remove_listener(listener)
     # Check that our signal is disconnected completely and maintains the same
     # value as the signal updates in the background
@@ -45,7 +47,6 @@ def test_invalid_signal():
     widget = WritableWidget()
     listener = widget.channels()[0]
     # Invalid Signal
-    sig_conn = SignalConnection("sig://not_my_signal",
-                                "not_my_signal")
+    sig_conn = SignalConnection(listener, 'my_signal')
     assert not widget._connected
     assert not widget._write_access
