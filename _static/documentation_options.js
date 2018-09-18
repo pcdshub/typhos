2308d11fd1cc02bf5f4ca0bd41a24a725f90ef64
Travis CI User <travis@example.org>
2018-09-18 21:24:50 +0000

    Update docs after building Travis build 154 of pcdshub/typhon
    The docs were built from the branch 'master' against the commit
    12096ba80719de6f6eef9694e259117b861671c2.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/typhon/jobs/430258643.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0+46.g7e855f1',
+    VERSION: '0.2.0+48.g12096ba',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
