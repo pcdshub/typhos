7b79c1462c3e9f11d2a2959d7dd72979b087af8a
teddyrendahl <trendahl@slac.stanford.edu>
2018-09-17 17:12:49 -0700

    TST: Add clean_name tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,24 @@
-from typhon.utils import use_stylesheet
+from ophyd import Device, Component as Cpt
+
+from typhon.utils import use_stylesheet, clean_name
+
+
+class NestedDevice(Device):
+    phi = Cpt(Device)
+
+
+class LayeredDevice(Device):
+    radial = Cpt(NestedDevice)
+
+
+def test_clean_name():
+    device = LayeredDevice(name='test')
+    assert clean_name(device.radial, strip_parent=False) == 'test radial'
+    assert clean_name(device.radial, strip_parent=True) == 'radial'
+    assert clean_name(device.radial.phi,
+                      strip_parent=False) == 'test radial phi'
+    assert clean_name(device.radial.phi, strip_parent=True) == 'phi'
+    assert clean_name(device.radial.phi, strip_parent=device) == 'radial phi'
 
 
 def test_stylesheet():
