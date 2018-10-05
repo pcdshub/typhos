2cecd2f87835df5f2460e3e88641f57ecb50276e
teddyrendahl <trendahl@slac.stanford.edu>
2018-10-05 15:08:54 -0700

    MAINT: Base TyphonTool object

---
+++ typhon/tools/core.py
@@ -0,0 +1,24 @@
+import logging
+
+from qtpy.QtWidgets import QWidget
+
+logger = logging.getLogger(__name__)
+
+
+class TyphonTool(QWidget):
+    """Mixin for all Typhon tool creation"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.devices = list()
+
+    def add_device(self, device):
+        """Add a new device to the tool"""
+        logger.debug("Adding device %s ...", device.name)
+        self.devices.append(device)
+
+    @classmethod
+    def from_device(cls, device, parent=None):
+        """Create a new instance of the tool for a Device"""
+        instance = cls(parent=parent)
+        instance.add_device(device)
+        return instance
