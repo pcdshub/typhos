c2b857ab7f8cf2bf986cb165159a151acc5a2e77
teddyrendahl <trendahl@slac.stanford.edu>
2018-10-06 00:01:15 -0700

    DOC: Improve TyphonTool documentation

--- typhon/tools/core.py
+++ typhon/tools/core.py
@@ -6,19 +6,40 @@ logger = logging.getLogger(__name__)
 
 
 class TyphonTool(QWidget):
-    """Mixin for all Typhon tool creation"""
+    """Base widget for all Typhon Tool creation"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.devices = list()
 
     def add_device(self, device):
-        """Add a new device to the tool"""
+        """
+        Add a new device to the tool
+
+        Parameters
+        ----------
+        device : ophyd.Device
+        """
         logger.debug("Adding device %s ...", device.name)
         self.devices.append(device)
 
     @classmethod
     def from_device(cls, device, parent=None, **kwargs):
-        """Create a new instance of the tool for a Device"""
+        """
+        Create a new instance of the tool for a Device
+
+        Shortcut for:
+
+        .. code::
+
+            tool = TyphonTool()
+            tool.add_device(device)
+
+        Parameters
+        ----------
+        device: ophyd.Device
+
+        parent: QWidget
+        """
         instance = cls(parent=parent, **kwargs)
         instance.add_device(device)
         return instance
