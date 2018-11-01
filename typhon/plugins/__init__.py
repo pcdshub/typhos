c8eb5417c216565e6e76c8856423c9ec7db7b412
teddyrendahl <trendahl@slac.stanford.edu>
2018-11-01 12:07:04 -0700

    MAINT: Register HappiPlugin on typhon import

--- typhon/plugins/__init__.py
+++ typhon/plugins/__init__.py
@@ -1,8 +1,11 @@
-__all__ = ['SignalPlugin', 'SignalConnection', 'register_signal']
+__all__ = ['SignalPlugin', 'SignalConnection', 'register_signal',
+           'HappiPlugin', 'HappiConnection', 'HappiChannel']
 
 from pydm.data_plugins import add_plugin
 
 from .core import SignalPlugin, SignalConnection, register_signal
+from .happi import HappiPlugin, HappiConnection, HappiChannel
 
-# Register SignalPlugin with PyDMApplication
+# Register custom plugins PyDMApplication
 add_plugin(SignalPlugin)
+add_plugin(HappiPlugin)
