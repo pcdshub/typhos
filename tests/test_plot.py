40a01d9412eff8a0f50d465794364f156b244e46
teddyrendahl <trendahl@slac.stanford.edu>
2018-11-09 12:05:16 -0800

    TST: Modify tests for new TimeChart internals

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -18,7 +18,7 @@ from qtpy.QtGui import QColor
 # Module #
 ##########
 from typhon import register_signal
-from typhon.tools.plot import ChannelDisplay, TyphonTimePlot, DeviceTimePlot
+from typhon.tools.plot import TyphonTimePlot, DeviceTimePlot
 
 @pytest.fixture(scope='session')
 def sim_signal():
@@ -26,14 +26,6 @@ def sim_signal():
     sim_sig.put(3.14)
     register_signal(sim_sig)
     return sim_sig
-
-
-def test_channeldisplay(qtbot):
-    disp = ChannelDisplay('Test Channel', QColor(Qt.white))
-    qtbot.addWidget(disp)
-    assert disp.ui.name.text() == 'Test Channel'
-    assert disp.ui.color.brush.color() == QColor(Qt.white)
-
 
 def test_add_signal(qtbot,sim_signal):
     # Create Signals
@@ -43,34 +35,27 @@ def test_add_signal(qtbot,sim_signal):
     qtbot.addWidget(ttp)
     # Add to list of available signals
     ttp.add_available_signal(epics_sig, 'Epics Signal')
-    assert ttp.ui.signal_combo.itemText(0) == 'Epics Signal'
-    assert ttp.ui.signal_combo.itemData(0) == 'ca://Tst:This'
+    assert ttp.signal_combo.itemText(0) == 'Epics Signal'
+    assert ttp.signal_combo.itemData(0) == 'ca://Tst:This'
     ttp.add_available_signal(sim_signal, 'Simulated Signal')
-    assert ttp.ui.signal_combo.itemText(1) == 'Simulated Signal'
-    assert ttp.ui.signal_combo.itemData(1) == 'sig://tst_this_2'
+    assert ttp.signal_combo.itemText(1) == 'Simulated Signal'
+    assert ttp.signal_combo.itemData(1) == 'sig://tst_this_2'
 
 
 def test_curve_methods(qtbot, sim_signal):
     ttp = TyphonTimePlot()
     qtbot.addWidget(ttp)
-    orig_color = ttp.ui.color.brush
     ttp.add_curve('sig://' + sim_signal.name, name=sim_signal.name)
     # Check that our signal is stored in the mapping
     assert sim_signal.name in ttp.channel_map
-    # Check that our next color is different
-    assert ttp.ui.color.brush != orig_color
-    # Check that our ChannelDisplay is live
-    assert ttp.ui.live_channels.layout().count() == 2  # 1 is a spacer
     # Check that our curve is live
-    assert len(ttp.ui.timeplot.curves) == 1
+    assert len(ttp.timechart.chart.curves) == 1
     # Try and add again
     ttp.add_curve('sig://' + sim_signal.name, name=sim_signal.name)
     # Check we didn't duplicate
-    assert ttp.ui.live_channels.layout().count() == 2  # 1 is a spacer
-    assert len(ttp.ui.timeplot.curves) == 1
+    assert len(ttp.timechart.chart.curves) == 1
     ttp.remove_curve(sim_signal.name)
-    assert ttp.ui.live_channels.layout().count() == 1  # 1 is a spacer
-    assert len(ttp.ui.timeplot.curves) == 0
+    assert len(ttp.timechart.chart.curves) == 0
 
 def test_curve_creation_button(qtbot, sim_signal):
     ttp = TyphonTimePlot()
@@ -79,12 +64,12 @@ def test_curve_creation_button(qtbot, sim_signal):
     ttp.creation_requested()
     # Check that our signal is stored in the mapping
     assert 'Sim Signal' in ttp.channel_map
-    assert len(ttp.ui.timeplot.curves) == 1
+    assert len(ttp.timechart.chart.curves) == 1
 
 def test_device_plot(motor, qtbot):
     dtp = TyphonTimePlot.from_device(motor)
     qtbot.addWidget(dtp)
     # Add the hint
-    assert len(dtp.ui.timeplot.curves) == 1
+    assert len(dtp.timechart.chart.curves) == 1
     # Added all the signals
-    assert dtp.ui.signal_combo.count() == len(motor.component_names)
+    assert dtp.signal_combo.count() == len(motor.component_names)
