0efd6bbfcd305ef24dd8a90dec66b4e62e2285e3
teddyrendahl <trendahl@slac.stanford.edu>
2018-11-26 09:15:36 -0800

    ENH: TyphonDisplayPlugin in designer

--- typhon/designer.py
+++ typhon/designer.py
@@ -3,9 +3,10 @@ import logging
 from pydm.widgets.qtplugin_base import qtplugin_factory
 
 from .signal import TyphonPanel
-
+from .display import TyphonDisplay
 
 logger = logging.getLogger(__name__)
 
 logging.info("Loading Typhon QtDesigner plugins ...")
 TyphonPanelPlugin = qtplugin_factory(TyphonPanel)
+TyphonDisplayPlugin = qtplugin_factory(TyphonDisplay)
