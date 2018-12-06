02d26af498e9fd4895be37f44b38348765505a8b
teddyrendahl <trendahl@slac.stanford.edu>
2018-12-05 16:31:23 -0800

    DEV: Utility script to setup QtDesigner environment variables for Typhon

---
+++ bin/typhon_env.sh
@@ -0,0 +1,5 @@
+#!/usr//bin/env bash
+TYPHON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export PYQTDESIGNERPATH="$TYPHON_DIR":$PYQTDESIGNERPATH
+export PYDM_DESIGNER_ONLINE=True
+
