ea495d95fb93d229280975b4d405d0b907b92400
teddyrendahl <trendahl@slac.stanford.edu>
2018-12-07 17:39:08 -0800

    TST: Refactor test suite into smaller chunks

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -5,6 +5,7 @@
 ############
 # External #
 ############
+import pytest
 from pyqtgraph.parametertree import ParameterTree, parameterTypes as ptypes
 from qtpy.QtWidgets import QDockWidget
 
@@ -16,31 +17,37 @@ from typhon.suite import TyphonSuite, DeviceParameter
 from .conftest import show_widget
 
 
-@show_widget
-def test_suite(device):
-    device.name = 'test'
+@pytest.fixture(scope='function')
+def suite(qtbot, device):
     suite = TyphonSuite.from_device(device)
-    assert device in suite.devices
-    # Grab all device displays
-    assert len(suite._device_group.childs) == 1
-    child_displays = suite._device_group.childs[0].childs
-    assert len(child_displays) == len(device._sub_devices)
-    # Default tools are loaded
-    assert len(suite.tools) == 3
-    assert len(suite.tools[0].devices) == 1
-    # No children
-    childless = TyphonSuite.from_device(device, children=False)
-    # Grab all device displays
-    childless_displays = childless._device_group.childs[0].childs
-    assert len(childless_displays) == 0
+    qtbot.addWidget(suite)
     return suite
 
 
 @show_widget
-def test_suite_subdisplay(qapp, qtbot, device):
-    # Set display by Device component
-    suite = TyphonSuite.from_device(device)
-    qtbot.addWidget(suite)
+def test_suite_with_child_devices(suite, device):
+    assert device in suite.devices
+    device_group = suite.top_level_groups[0]
+    assert len(device_group.childs) == 1
+    child_displays = device_group.childs[0].childs
+    assert len(child_displays) == len(device._sub_devices)
+    return suite
+
+def test_suite_without_children(device, qtbot):
+    childless = TyphonSuite.from_device(device, children=False)
+    qtbot.addWidget(childless)
+    device_group = childless.top_level_groups[0]
+    childless_displays = device_group.childs[0].childs
+    assert len(childless_displays) == 0
+
+
+def test_suite_tools(suite):
+    assert len(suite.tools) == 3
+    assert len(suite.tools[0].devices) == 1
+
+
+def test_suite_subdisplay(qtbot, suite):
+    device = suite.devices[0]
     x_display = suite.get_subdisplay(device.x)
     assert device.x in x_display.devices
     suite.show_subdisplay(device.x)
