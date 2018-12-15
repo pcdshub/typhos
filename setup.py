4e0763ad5ec5d65bafa71062a5400d2d2b6ac69a
teddyrendahl <trendahl@slac.stanford.edu>
2018-12-14 17:06:26 -0800

    FIX: Printed message in setup was non-sensical

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().split()
 
-requirements = [r for r in requirements if not r.startswith('git+')]
 git_requirements = [r for r in requirements if r.startswith('git+')]
-print("User must install \n" +
-      "\n".join(f' {r}' for r in git_requirements) +
-      "\n\nmanually")
-
+requirements = [r for r in requirements if not r.startswith('git+')]
+print("User must install the following packages manually:\n" +
+      "\n".join(f' {r}' for r in git_requirements))
 
 setup(name='typhon',
       version=versioneer.get_version(),
