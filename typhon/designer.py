a351664ce3f35ddaa4028da7fd3fc6f481f684c3
teddyrendahl <trendahl@slac.stanford.edu>
2018-12-18 16:32:37 -0800

    MAINT: Add TyphonMethodButton to Designer plugins

--- typhon/designer.py
+++ typhon/designer.py
@@ -4,9 +4,10 @@ from pydm.widgets.qtplugin_base import qtplugin_factory
 
 from .signal import TyphonPanel
 from .display import TyphonDisplay
-
+from .func import TyphonMethodButton
 logger = logging.getLogger(__name__)
 
 logging.info("Loading Typhon QtDesigner plugins ...")
 TyphonPanelPlugin = qtplugin_factory(TyphonPanel)
 TyphonDisplayPlugin = qtplugin_factory(TyphonDisplay)
+TyphonMethodButtonPlugin = qtplugin_factory(TyphonMethodButton)
