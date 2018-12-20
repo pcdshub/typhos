64908f2d4f265f5bf073722f2fabc16554db26ea
teddyrendahl <trendahl@slac.stanford.edu>
2018-12-20 15:50:00 -0800

    MAINT: HappiPlugin is optional

--- typhon/plugins/__init__.py
+++ typhon/plugins/__init__.py
@@ -1,11 +1,17 @@
 __all__ = ['SignalPlugin', 'SignalConnection', 'register_signal',
            'HappiPlugin', 'HappiConnection', 'HappiChannel']
+import logging
 
 from pydm.data_plugins import add_plugin
 
 from .core import SignalPlugin, SignalConnection, register_signal
-from .happi import HappiPlugin, HappiConnection, HappiChannel
 
-# Register custom plugins PyDMApplication
+
+logger = logging.getLogger(__name__)
 add_plugin(SignalPlugin)
-add_plugin(HappiPlugin)
+
+try:
+    from .happi import HappiPlugin, HappiConnection, HappiChannel
+    add_plugin(HappiPlugin)
+except ImportError:
+    logger.warning("Unable to import HappiPlugin")
