8c6d8531dc313d1e492a734e655a559f63d88a44
teddyrendahl <trendahl@slac.stanford.edu>
2019-01-10 14:08:37 -0800

    MAINT: Place Typhon widgets in their own Designer group

--- typhon/designer.py
+++ typhon/designer.py
@@ -8,9 +8,14 @@ from .func import TyphonMethodButton
 from .positioner import TyphonPositionerWidget
 
 logger = logging.getLogger(__name__)
+logger.info("Loading Typhon QtDesigner plugins ...")
 
-logging.info("Loading Typhon QtDesigner plugins ...")
-TyphonSignalPanelPlugin = qtplugin_factory(TyphonSignalPanel)
-TyphonDeviceDisplayPlugin = qtplugin_factory(TyphonDeviceDisplay)
-TyphonMethodButtonPlugin = qtplugin_factory(TyphonMethodButton)
-TyphonPositionerWidgetPlugin = qtplugin_factory(TyphonPositionerWidget)
+group_name = 'Typhon Widgets'
+TyphonSignalPanelPlugin = qtplugin_factory(TyphonSignalPanel,
+                                           group=group_name)
+TyphonDeviceDisplayPlugin = qtplugin_factory(TyphonDeviceDisplay,
+                                             group=group_name)
+TyphonMethodButtonPlugin = qtplugin_factory(TyphonMethodButton,
+                                            group=group_name)
+TyphonPositionerWidgetPlugin = qtplugin_factory(TyphonPositionerWidget,
+                                                group=group_name)
