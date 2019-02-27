5d4c4ef7ae9813de9995227a94b9e46c4c647a5d
K Lauer <klauer@slac.stanford.edu>
2019-02-27 14:17:49 -0800

    REF: continued work on PortGraphMonitor

--- typhon/port_graph.py
+++ typhon/port_graph.py
@@ -1,12 +1,18 @@
+import logging
 import os
+import threading
+
 from pyqtgraph.flowchart import (Flowchart, Node, NodeGraphicsItem,
                                  FlowchartWidget)
 from pyqtgraph.flowchart.library import NodeLibrary
-import pyqtgraph.widgets as pyqtgraph_widgets
+import pyqtgraph.widgets as qtg_widgets
 
 from qtpy import QtWidgets, QtGui, QtCore
 
 from ophyd import SimDetector, Component as Cpt, CommonPlugins_V32, CamBase
+
+
+logger = logging.getLogger(__name__)
 
 
 class PortNodeItem(NodeGraphicsItem):
@@ -41,11 +47,9 @@ class PortNode(Node):
         super().__init__(name, terminals=terminals, allowRemove=False)
 
     def process(self, **kwds):
-        print('process!', self)
         return {'Out': 0}
 
     def processBypassed(self, args):
-        print('bypassed', args, self)
         return super().processBypassed(args)
 
     def graphicsItem(self):
@@ -63,7 +67,7 @@ class Library(NodeLibrary):
         ...
 
 
-class PortTreeWidget(pyqtgraph_widgets.TreeWidget.TreeWidget):
+class PortTreeWidget(qtg_widgets.TreeWidget.TreeWidget):
     @QtCore.Slot()
     def reorder(self):
         ...
@@ -74,8 +78,8 @@ class PortGraphControlWidget(QtWidgets.QWidget):
     The widget that contains the list of all the nodes in a flowchart and their
     controls, as well as buttons for loading/saving flowcharts.
 
-    ((WIP reimplementation of FlowChartCtrlWidget))
     '''
+    # ((WIP reimplementation of FlowChartCtrlWidget))
 
     def __init__(self, chart):
         self.items = {}
@@ -88,7 +92,7 @@ class PortGraphControlWidget(QtWidgets.QWidget):
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.setVerticalSpacing(0)
 
-        self.reload_button = pyqtgraph_widgets.FeedbackButton.FeedbackButton(self)
+        self.reload_button = qtg_widgets.FeedbackButton.FeedbackButton(self)
         self.reload_button.setText('Reload')
         self.reload_button.setCheckable(False)
         self.reload_button.setFlat(False)
@@ -180,20 +184,152 @@ class PortGraphControlWidget(QtWidgets.QWidget):
         self.tree.setCurrentItem(item)
 
 
+class PortGraphMonitor(QtCore.QObject):
+    edge_added = QtCore.Signal(str, str)
+    edge_removed = QtCore.Signal(str, str)
+    port_added = QtCore.Signal(str)
+    port_removed = QtCore.Signal(str)
+    update = QtCore.Signal(list, list, list, list)
+
+    def __init__(self, detector, parent=None):
+        super().__init__(parent=parent)
+        self.digraph = None
+        self.port_dict = {}
+        self.edges = set()
+        self.positions = {}
+        self.detector = detector
+        self._subscriptions = {}
+        self.lock = threading.Lock()
+
+    @property
+    def cameras(self):
+        'All camera port names'
+        return [port
+                for port, plugin in self.port_dict.items()
+                if isinstance(plugin, CamBase)]
+
+    def _port_changed_callback(self, value=None, obj=None, **kwargs):
+        logger.debug('Source port of %s changed to %s', obj.name, value)
+        self.update_ports()
+
+    def update_ports(self):
+        'Read the port digraph/dictionary from the detector and emit updates'
+        self.detector.wait_for_connection()
+        digraph, port_dict = self.detector.get_asyn_digraph()
+
+        with self.lock:
+            for port, plugin in sorted(port_dict.items()):
+                if (port not in self._subscriptions and
+                        hasattr(plugin, 'nd_array_port')):
+                    logger.debug('Subscribing to port %s (%s) NDArrayPort',
+                                 port, plugin.name)
+                    self._subscriptions[port] = plugin.nd_array_port.subscribe(
+                        self._port_changed_callback, run=False)
+                # plugin_type, ad_core_version, driver_version
+
+            edges = list(sorted(digraph.edges))
+            ports_removed = list(sorted(set(self.port_dict) - set(port_dict)))
+            ports_added = list(sorted(set(port_dict) - set(self.port_dict)))
+            edges_removed = list(sorted(set(self.edges) - set(edges)))
+            edges_added = list(sorted((set(edges) - set(self.edges))))
+
+            self.digraph = digraph
+            self.edges = edges
+            self.port_dict.clear()
+            self.port_dict.update(**port_dict)
+
+            for port in ports_removed:
+                sub = self._subscriptions.pop(port, None)
+                if sub is not None:
+                    plugin = port_dict[port].nd_array_port.unsubscribe(sub)
+
+        for port in ports_removed:
+            self.port_removed.emit(port)
+
+        for port in ports_added:
+            self.port_added.emit(port)
+
+        for src, dest in edges_removed:
+            self.edge_removed.emit(src, dest)
+
+        for src, dest in edges_added:
+            self.edge_added.emit(src, dest)
+
+        if ports_removed or ports_added or edges_removed or edges_added:
+            self.update.emit(ports_removed, ports_added, edges_removed,
+                             edges_added)
+
+
 class PortGraphFlowchart(Flowchart):
     def __init__(self, detector, library):
         super().__init__(terminals={},
                          library=library)
+        self._widget = None
 
         # Unused input/output widgets:
         for node in (self.inputNode, self.outputNode):
             self.removeNode(node)
 
-        self.port_dict = None
-        self.digraph = None
-        self.detector = detector
-        self.nodes = {}
-        self._widget = None
+        self.monitor = PortGraphMonitor(detector, parent=self)
+
+        self.monitor.update.connect(self._ports_updated)
+        self._nodes = {}
+        self._edges = set()
+        self._auto_position = True
+
+    def _ports_updated(self, ports_removed, ports_added, edges_removed,
+                       edges_added):
+        self.port_dict = self.monitor.port_dict
+
+        for src, dest in edges_removed:
+            try:
+                src_node = self._nodes[src]['node']
+                dest_node = self._nodes[dest]['node']
+            except KeyError:
+                logger.debug('Edge removed that did not connect a known port, '
+                             'likely in error: %s -> %s', src, dest)
+                continue
+
+            src_node['Out'].disconnectFrom(dest_node['In'])
+            self._edges.remove((src, dest))
+
+        for port in ports_removed:
+            node = self._nodes.pop(port)
+            node.disconnectAll()
+            self.removeNode(node)
+
+        for port in ports_added:
+            plugin = self.port_dict[port]
+            self._nodes[port] = dict(node=self.add_port(port, plugin),
+                                     plugin=plugin)
+
+        for src, dest in edges_added:
+            try:
+                src_node = self._nodes[src]['node']
+                dest_node = self._nodes[dest]['node']
+            except KeyError:
+                # Scenarios:
+                #  1. Invalid port name used
+                #  2. Associated plugin missing from the Detector class
+                logger.debug('Edge added to unknown port: %s -> %s', src, dest)
+                continue
+
+            try:
+                if src_node != dest_node:
+                    self.connectTerminals(src_node['Out'], dest_node['In'])
+            except Exception:
+                logger.exception('Failed to connect terminals %s -> %s', src,
+                                 dest)
+
+            self._edges.add((src, dest))
+
+        if self._auto_position:
+            positions = position_nodes(self._edges, self.port_dict)
+            for port, (px, py) in positions.items():
+                node = self._nodes[port]['node']
+                node.graphicsItem().setPos(px, py)
+
+            self.widget().chartWidget.view.scale(1, 1)
 
     def widget(self):
         """Return the control widget for this flowchart.
@@ -207,52 +343,22 @@ class PortGraphFlowchart(Flowchart):
             self.viewBox = self._widget.viewBox()
         return self._widget
 
-    def add_port(self, name, plugin, pos):
+    def add_port(self, name, plugin, pos=None):
         has_input = not isinstance(plugin, CamBase)
         node = PortNode(name, has_input=has_input)
-        self.addNode(node, name, pos)
+        self.addNode(node, name, pos=pos)
         return node
 
-    @property
-    def cameras(self):
-        'All camera port names'
-        return [port
-                for port, plugin in self.port_dict.items()
-                if isinstance(plugin, CamBase)]
 
-    def read_ports(self):
-        'Read the port digraph/dictionary from the detector'
-        self.detector.wait_for_connection()
-        self.digraph, self.port_dict = self.detector.get_asyn_digraph()
-
-        self.nodes = {
-            port: {'position': pos}
-            for port, pos in
-            position_nodes(self.digraph, self.port_dict).items()
-        }
-
-        for port, plugin in sorted(self.port_dict.items()):
-            node_info = self.nodes[port]
-            node_info['node'] = self.add_port(port, plugin,
-                                              node_info['position'])
-
-        for src, dest in self.digraph.edges:
-            self.connectTerminals(self.nodes[src]['node']['Out'],
-                                  self.nodes[dest]['node']['In'])
-
-        # for port in self.cameras:
-        #     cam = self.nodes[port]['node']
-        #     cam.setInput(Out='test')
-
-
-def position_nodes(digraph, port_dict, *, x_spacing=PortNodeItem.WIDTH * 1.5,
+def position_nodes(edges, port_dict, *, x_spacing=PortNodeItem.WIDTH * 1.5,
                    y_spacing=PortNodeItem.HEIGHT * 1.5, x=0, y=0):
     '''
     Generate an (x, y) position dictionary for all nodes in the port dictionary
 
     Parameters
     ----------
-    digraph : networkx.Digraph
+    edges : list of (src, dest)
+        Digraph edges that connect source -> destination ports
     port_dict : dict
         Dictionary of port name to ophyd plugin
     x_spacing : float, optional
@@ -266,8 +372,9 @@ def position_nodes(digraph, port_dict, *, x_spacing=PortNodeItem.WIDTH * 1.5,
     '''
     def position_port(port, x, y):
         positions[port] = (x, y)
-        dests = [dest for src, dest in digraph.edges
-                 if src == port]
+        dests = [dest for src, dest in edges
+                 if src == port
+                 and src != dest]
         y -= y_spacing * (len(dests) // 2)
         for idx, dest in enumerate(sorted(dests)):
             position_port(dest, x + x_spacing, y + idx * y_spacing)
@@ -286,7 +393,8 @@ def position_nodes(digraph, port_dict, *, x_spacing=PortNodeItem.WIDTH * 1.5,
 
     # Add any ports that are otherwise unconnected
     x = start_x
-    y = y_spacing + max(y for x, y in positions.values())
+    if positions:
+        y = y_spacing + max(y for x, y in positions.values())
 
     for port in port_dict:
         if port not in positions:
@@ -303,7 +411,7 @@ def test():
 
     fc = PortGraphFlowchart(detector=det, library=Library())
 
-    fc.read_ports()
+    fc.monitor.update_ports()
 
     w = fc.widget()
     # layout.addWidget(fc.widget(), 0, 0, 2, 1)
@@ -313,6 +421,8 @@ def test():
 
 if __name__ == '__main__':
     import sys
+    logging.basicConfig()
+    logger.setLevel('DEBUG')
     app = QtGui.QApplication([])
 
     win = QtGui.QMainWindow()
