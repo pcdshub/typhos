82d8d272281e7e715904d3aa2572ae43a5cce3e5
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-03-08 17:29:44 +0000

    Update docs after building Travis build 332 of pcdshub/typhon
    The docs were built from the branch 'master' against the commit
    30ed39dc5cf019df6ad899fef9f803a245177603.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/typhon/jobs/503705204.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+47.gb469177',
+    VERSION: '0.3.0+50.g30ed39d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
