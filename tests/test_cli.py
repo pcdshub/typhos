adcc93662c6bf53a71a1b303b182c73f99988623
teddyrendahl <trendahl@slac.stanford.edu>
2019-03-20 10:11:18 -0700

    TST: Tests expect QMainWindow returned instead of TyphonSuite

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,30 +15,31 @@ def test_cli_version(capsys):
 
 def test_cli_happi_cfg(monkeypatch, qtbot, happi_cfg):
     monkeypatch.setattr(QApplication, 'exec_', lambda x: 1)
-    suite = typhon_cli(['test_motor', '--happi-cfg', happi_cfg])
-    qtbot.addWidget(suite)
-    assert suite.isVisible()
-    assert 'test_motor' == suite.devices[0].name
+    window = typhon_cli(['test_motor', '--happi-cfg', happi_cfg])
+    qtbot.addWidget(window)
+    assert window.isVisible()
+    assert 'test_motor' == window.centralWidget().devices[0].name
 
 def test_cli_bad_entry(qtbot, happi_cfg):
-    suite = typhon_cli(['no_motor', '--happi-cfg', happi_cfg])
-    assert suite is None
+    window = typhon_cli(['no_motor', '--happi-cfg', happi_cfg])
+    assert window is None
 
 def test_cli_no_entry(monkeypatch, qtbot, happi_cfg):
     monkeypatch.setattr(QApplication, 'exec_', lambda x: 1)
-    suite = typhon_cli(['--happi-cfg', happi_cfg])
-    qtbot.addWidget(suite)
-    assert suite.isVisible()
-    assert suite.devices == []
+    window = typhon_cli(['--happi-cfg', happi_cfg])
+    qtbot.addWidget(window)
+    assert window.isVisible()
+    assert window.centralWidget().devices == []
 
 def test_cli_stylesheet(monkeypatch, qapp, qtbot, happi_cfg):
     monkeypatch.setattr(QApplication, 'exec_', lambda x: 1)
     with open('test.qss', 'w+') as handle:
         handle.write("TyphonDeviceDisplay {qproperty-force_template: 'test.ui'}")
     style = qapp.styleSheet()
-    suite = typhon_cli(['test_motor', '--stylesheet', 'test.qss',
+    window = typhon_cli(['test_motor', '--stylesheet', 'test.qss',
                         '--happi-cfg', happi_cfg])
-    qtbot.addWidget(suite)
+    qtbot.addWidget(window)
+    suite = window.centralWidget()
     dev_display = suite.get_subdisplay(suite.devices[0])
     assert dev_display.force_template == 'test.ui'
     qapp.setStyleSheet(style)
