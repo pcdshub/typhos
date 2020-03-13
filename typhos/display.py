32db86ef0dd3cb0142b492098f1e920f41752021
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-03-12 17:30:32 -0700

    FIX: Fix QGridLayout: Multi-cell fromRow greater than toRow error.

--- typhos/display.py
+++ typhos/display.py
@@ -166,7 +166,7 @@ class TyphosDisplayTitle(QtWidgets.QFrame, widgets.TyphosDesignerMixin):
         self.grid_layout = QtWidgets.QGridLayout()
         self.grid_layout.addWidget(self.label, 0, 0)
         self.grid_layout.addWidget(self.switcher, 0, 1, Qt.AlignRight)
-        self.grid_layout.addWidget(self.underline, 1, 0, 0, 2)
+        self.grid_layout.addWidget(self.underline, 1, 0, 1, 2)
         self.setLayout(self.grid_layout)
 
         # Set the property:
