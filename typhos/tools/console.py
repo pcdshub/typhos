b94e8fb5204740be8059baefef14aad2a0c36cd4
K Lauer <klauer@slac.stanford.edu>
2020-04-13 16:22:14 -0700

    REF: rework jupyter widget creation

--- typhos/tools/console.py
+++ typhos/tools/console.py
@@ -3,11 +3,32 @@ import threading
 
 from qtconsole.manager import QtKernelManager
 from qtconsole.rich_jupyter_widget import RichJupyterWidget
-from qtpy.QtWidgets import QApplication, QHBoxLayout
+from qtpy import QtWidgets
 
 from .. import utils
 
 logger = logging.getLogger(__name__)
+
+
+def _make_jupyter_widget_with_kernel(kernel_name):
+    """
+    Start a kernel, connect to it, and create a RichJupyterWidget to use it
+
+    Parameters
+    ----------
+    kernel_name : str
+        Kernel name to use
+    """
+    kernel_manager = QtKernelManager(kernel_name=kernel_name)
+    kernel_manager.start_kernel()
+
+    kernel_client = kernel_manager.client()
+    kernel_client.start_channels()
+
+    jupyter_widget = RichJupyterWidget()
+    jupyter_widget.kernel_manager = kernel_manager
+    jupyter_widget.kernel_client = kernel_client
+    return jupyter_widget
 
 
 class TyphosConsole(utils.TyphosBase):
@@ -27,27 +48,20 @@ class TyphosConsole(utils.TyphosBase):
     """
     def __init__(self, parent=None):
         super().__init__(parent=parent)
+        self._shutting_down = False
+
         # Setup widget
-        self.kernel = RichJupyterWidget()
-        self.setLayout(QHBoxLayout())
+        self.jupyter_widget = _make_jupyter_widget_with_kernel('python3')
+        self.jupyter_widget.syntax_style = 'monokai'
+        self.jupyter_widget.set_default_style(colors='Linux')
+
+        # Set the layout
+        self.setLayout(QtWidgets.QHBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
-        self.layout().addWidget(self.kernel)
-        # Create a Kernel
-        logger.debug("Starting Jupyter Kernel ...")
-        kernel_manager = QtKernelManager(kernel_name='python3')
-        kernel_manager.start_kernel()
-        kernel_client = kernel_manager.client()
-        kernel_client.start_channels()
-        self.kernel.kernel_manager = kernel_manager
-        self.kernel.kernel_client = kernel_client
+        self.layout().addWidget(self.jupyter_widget)
+
         # Ensure we shutdown the kernel
-        app = QApplication.instance()
-        app.aboutToQuit.connect(self.shutdown)
-        # Styling
-        self.kernel.syntax_style = 'monokai'
-        self.kernel.set_default_style(colors='Linux')
-        # Ensure cleanup
-        app = QApplication.instance()
+        app = QtWidgets.QApplication.instance()
         app.aboutToQuit.connect(self.shutdown)
 
     def sizeHint(self):
@@ -57,20 +71,24 @@ class TyphosConsole(utils.TyphosBase):
 
     def shutdown(self):
         """Shutdown the Jupyter Kernel"""
-        client = self.kernel.kernel_client
-        if client.channels_running:
-            logger.debug("Stopping Jupyter Client")
-            # Stop channels in the background
-            t = threading.Thread(target=client.stop_channels)
-            t.start()
-            self.kernel.kernel_manager.shutdown_kernel()
-        else:
+        client = self.jupyter_widget.kernel_client
+        if not self._shutting_down:
             logger.debug("Kernel is already shutdown.")
+            return
+
+        self._shutting_down = True
+        logger.debug("Stopping Jupyter Client")
+
+        def cleanup():
+            client.stop_channels()
+            self.kernel_manager.shutdown_kernel()
+
+        threading.Thread(target=cleanup, daemon=True).start()
 
     def add_device(self, device):
         try:
-            load_script = utils.code_from_device(device)
-            self.kernel.kernel_client.execute(load_script, silent=False)
+            script = utils.code_from_device(device)
+            self.jupyter_widget.execute(script, hidden=False, interactive=True)
         except Exception:
             logger.exception("Unable to add device %r to TyphosConsole.",
                              device.name)
