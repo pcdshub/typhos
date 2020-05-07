63de296d17e7f0a44499b93ecdd53837af84eb7c
Zachary Lentz <zlentz@slac.stanford.edu>
2020-05-07 15:52:39 -0700

    ENH: expand test device maker to nested devices

--- typhos/benchmark/device.py
+++ typhos/benchmark/device.py
@@ -5,16 +5,17 @@ These are meant to be used for targetted benchmarking and typically are
 "extreme" in both size and in composition in order to make specific
 loading issues more obvious.
 
-Note that this currently only supports simple, flat devices.
-In the future it can be expanded to have complicated nesting devices,
-Kind information, different data types to test different widget types, etc.
+Note that this currently only supports devices with uniform signals.
+In the future it can be expanded to have Kind information, different
+data types to test different widget types, etc.
 """
 from ophyd.device import Device, Component as Cpt
 from ophyd.signal import Signal
 
 
 def make_test_device_class(name='TestClass', signal_class=Signal,
-                           include_prefix=False, num_signals=10):
+                           include_prefix=False, num_signals=10,
+                           subdevice_layers=0, subdevice_spread=0):
     """
     Creates a test :class:`ophyd.Device` subclass.
 
@@ -36,8 +37,24 @@ def make_test_device_class(name='TestClass', signal_class=Signal,
         Defaults to False.
 
     num_signals : int, optional
-        The number of signals to use in the test class.
+        The number of signals to use in the test class. Note that this is the
+        number of signals per bottom-level subdevice. Therefore, the actual
+        total number of signals can be some multiple of this number.
         Defaults to 10.
+
+    subdevice_layers : int, optional
+        The number of subdevices we need to traverse down before seeing
+        signals. For example, putting this at 0 results in no subdevices and
+        all signals at the top level, while putting this at 2 gives us only
+        subdevices at the top level, only subdevices on each of these
+        subdevices, and only signals on these bottom-most subdevices.
+        Has no effect if subdevice_spread is 0.
+        Defaults to 0.
+
+    subdevice_spread : int, optional
+        The number of subdevices to include in each layer.
+        Has no effect if subdevice_layers is 0.
+        Defaults to 0
     """
     signals = {}
     for nsig in range(num_signals):
@@ -47,4 +64,18 @@ def make_test_device_class(name='TestClass', signal_class=Signal,
             sig_cpt = Cpt(signal_class)
         signals[f'signum{nsig}'] = sig_cpt
 
-    return type(name, (Device,), signals)
+    SignalHolder = type('SignalHolder', (Device,), signals)
+
+    if all((subdevice_layers > 0, subdevice_spread > 0)):
+        PrevClass = SignalHolder
+        while subdevice_layers > 0:
+            subdevices = {}
+            for ndev in range(subdevice_spread):
+                subdevices[f'devnum{ndev}'] = Cpt(PrevClass, f'PREFIX{ndev}:')
+            ThisClass = type(f'Layer{subdevice_layers}', (Device,), subdevices)
+            PrevClass = ThisClass
+            subdevice_layers -= 1
+    else:
+        ThisClass = SignalHolder
+
+    return type(name, (ThisClass,), {})
