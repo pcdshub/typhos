1e9c1968fef464ca6154e585c800b0320a3c036c
Zachary Lentz <zlentz@slac.stanford.edu>
2020-05-07 16:27:57 -0700

    ENH: expand caproto ioc tester to handle nested classes

--- typhos/benchmark/cases.py
+++ typhos/benchmark/cases.py
@@ -48,19 +48,46 @@ def run_caproto_ioc(device_class, prefix):
     Includes all the PVs that device_class will have if instantiate with
     prefix.
 
-    Currently only works for flat devices, e.g. no subdevices
+    Assumes only basic :class:`ophyd.Component` instances in the class
+    definition.
     """
     pvprops = {}
-    for walk in device_class.walk_components():
-        cpt = walk.item
-        if issubclass(cpt.cls, EpicsSignalBase):
-            pvprops[cpt.suffix] = pvproperty()
+    for suffix in yield_all_suffixes(device_class):
+        pvprops[suffix] = pvproperty()
 
     DynIOC = type('DynIOC', (PVGroup,), pvprops)
     ioc = DynIOC(prefix)
 
     run(ioc.pvdb, module_name='caproto.asyncio.server',
         interfaces=['0.0.0.0'])
+
+
+def yield_all_suffixes(device_class):
+    """
+    Iterates through all full pvname suffixes defined by device_class.
+
+    Assumes only basic :class:`ophyd.Component` instances in the class
+    definition.
+    """
+    for walk in device_class.walk_components():
+        if issubclass(walk.item.cls, EpicsSignalBase):
+            suffix = get_suffix(walk)
+            yield suffix
+
+
+def get_suffix(walk):
+    """
+    Returns the full pvname suffix from a ComponentWalk instance
+
+    This means everything after the top-level device's prefix.
+    Assumes that walk is an :class:`ophyd.signal.EpicsSignalBase`
+    subclass and that it was defined using only
+    :class:`ophyd.Component` in the device ancestors tree.
+    """
+    suffix = ''
+    for cls, attr in zip(walk.ancestors, walk.dotted_name.split('.')):
+        suffix += getattr(cls, attr).suffix
+    return suffix
 
 
 @contextmanager
