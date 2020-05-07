e30c0f5b057518ae45e595515d4effc63587aa52
Zachary Lentz <zlentz@slac.stanford.edu>
2020-05-07 15:15:20 -0700

    TST: add pytest-benchmark to test suite

---
+++ tests/test_benchmark.py
@@ -0,0 +1,17 @@
+"""
+Run the benchmark test cases using pytest-benchmark
+"""
+import pytest
+
+from typhos.benchmark.cases import benchmark_registry
+
+
+# Name the test cases using the keys, run using the values
+@pytest.mark.parametrize('test_function', benchmark_registry.keys())
+def test_benchmark(test_function, benchmark):
+    """
+    Run all registered benchmarks.
+
+    These typically just open and close a particular typhos screen.
+    """
+    benchmark(benchmark_registry[test_function])
