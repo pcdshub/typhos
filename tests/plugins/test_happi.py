00945ac75d0aac8737c0ba5ef2db1a392daeff00
K Lauer <klauer@slac.stanford.edu>
2020-05-07 14:00:39 -0700

    TST: fix tests for Qt.QueuedConnection

--- tests/plugins/test_happi.py
+++ tests/plugins/test_happi.py
@@ -9,13 +9,18 @@ from typhos.plugins.happi import HappiPlugin
 from typhos.widgets import HappiChannel
 
 
-def test_connection(client):
+def test_connection(qtbot, client):
     hp = HappiPlugin()
     # Register a channel and check we received object and metadata
     mock = Mock()
     hc = HappiChannel(address='happi://test_device', tx_slot=mock)
     hp.add_connection(hc)
-    assert mock.called
+
+    def mock_called():
+        assert mock.called
+
+    qtbot.wait_until(mock_called)
+
     tx = mock.call_args[0][0]
     assert isinstance(tx['obj'], ophyd.sim.SynAxis)
     assert isinstance(tx['md'], dict)
@@ -23,7 +28,11 @@ def test_connection(client):
     mock2 = Mock()
     hc2 = HappiChannel(address='happi://test_device', tx_slot=mock2)
     hp.add_connection(hc2)
-    assert mock2.called
+
+    def mock2_called():
+        assert mock2.called
+
+    qtbot.wait_until(mock2_called)
     mock.assert_called_once()
     # Disconnect
     hp.remove_connection(hc)
@@ -31,11 +40,16 @@ def test_connection(client):
     assert hp.connections == {}
 
 
-def test_connection_for_child(client):
+def test_connection_for_child(qtbot, client):
     hp = HappiPlugin()
     mock = Mock()
     hc = HappiChannel(address='happi://test_motor.setpoint', tx_slot=mock)
     hp.add_connection(hc)
+
+    def mock_called():
+        assert mock.called
+
+    qtbot.wait_until(mock_called)
     tx = mock.call_args[0][0]
     assert tx['obj'].name == 'test_motor_setpoint'
 
