7a019aca64d8b519ff38b8d9b1f3fec7ab556899
Zachary Lentz <zlentz@slac.stanford.edu>
2020-05-08 12:12:48 -0700

    CI: switch to the skip and only args for better separation

--- run_tests.py
+++ run_tests.py
@@ -10,8 +10,8 @@ import pytest
 if __name__ == '__main__':
     # Show output results from every test function
     # Show the message output for skipped and expected failures
-    # Skip the benchmarking re-runs
-    args = ['-v', '-vrxs', '--benchmark-disable']
+    # Skip the benchmarking
+    args = ['-v', '-vrxs', '--benchmark-skip']
 
     # Add extra arguments
     if len(sys.argv) > 1:
