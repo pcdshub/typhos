8be4867bbf801371d4c05a03d621b26c59d781b6
Zachary Lentz <zlentz@slac.stanford.edu>
2020-05-13 16:53:16 -0700

    TST: crude live patch for test cleanup

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -3,6 +3,7 @@ Run the benchmark test cases using pytest-benchmark
 """
 import pytest
 
+from epics import PV
 import typhos.benchmark.utils as utils
 from typhos.benchmark.cases import unit_tests
 from typhos.benchmark.profile import profiler_context
@@ -12,12 +13,14 @@ from .conftest import save_image
 
 # Name the test cases using the keys, run using the values
 @pytest.mark.parametrize('unit_test_name', unit_tests.keys())
-def test_benchmark(unit_test_name, qapp, qtbot, benchmark):
+def test_benchmark(unit_test_name, qapp, qtbot, benchmark, monkeypatch):
     """
     Run all registered benchmarks.
 
     These typically just open and close a particular typhos screen.
     """
+    # Crudely permenant patch here to get around cleanup bug
+    PV.count = property(lambda self: 1)
     suite = benchmark(inner_benchmark, unit_tests[unit_test_name], qtbot)
     save_image(suite, 'test_benchmark_' + unit_test_name)
 
