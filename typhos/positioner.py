335a5a57339e11704825abac2e7708a8578b717f
K Lauer <klauer@slac.stanford.edu>
2020-05-13 14:01:53 -0700

    REF: use a decorator helper to link device + widgets

--- typhos/positioner.py
+++ typhos/positioner.py
@@ -1,18 +1,66 @@
+import functools
 import logging
+import operator
 import os.path
 
 from qtpy import uic
 from qtpy.QtCore import Property, Slot
 
-from ophyd import Device
-
 from .plugins import register_signal
 from .status import TyphosStatusThread
-from .utils import (TyphosBase, channel_from_signal, grab_kind,
-                    raise_to_operator, reload_widget_stylesheet, ui_dir)
+from .utils import (TyphosBase, channel_from_signal, raise_to_operator,
+                    reload_widget_stylesheet, ui_dir)
 from .widgets import TyphosDesignerMixin
 
 logger = logging.getLogger(__name__)
+
+
+def _linked_attribute(property_attr, widget_attr):
+    """
+    Decorator which connects a device signal with a widget.
+
+    Retrieves the signal from the device, registers it with PyDM, and sets the
+    widget channel.
+
+    Parameters
+    ----------
+    property_attr : str
+        This is one level of indirection, allowing for the component attribute
+        to be configurable by way of designable properties.
+        In short, this looks like:
+            ``getattr(self.device, getattr(self, property_attr))``
+        The component attribute name may include multiple levels (e.g.,
+        ``'cpt1.cpt2.low_limit'``).
+
+    widget_attr : str
+        The attribute name of the widget, referenced from ``self``.
+        The component attribute name may include multiple levels (e.g.,
+        ``'ui.low_limit'``).
+    """
+    get_widget_attr = operator.attrgetter(widget_attr)
+
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped(self):
+            widget = get_widget_attr(self)
+            device_attr = getattr(self, property_attr)
+            get_device_attr = operator.attrgetter(device_attr)
+
+            try:
+                signal = get_device_attr(self.device)
+            except AttributeError:
+                signal = None
+            else:
+                register_signal(signal)
+                if widget is not None:
+                    widget.channel = channel_from_signal(signal)
+
+            logger.debug('device.%s => self.%s (signal: %s widget: %s)',
+                         device_attr, widget_attr, signal, widget)
+            return func(self, signal, widget)
+
+        return wrapped
+    return wrapper
 
 
 class TyphosPositionerWidget(TyphosBase, TyphosDesignerMixin):
@@ -31,16 +79,18 @@ class TyphosPositionerWidget(TyphosBase, TyphosDesignerMixin):
     ============== ===========================================================
     Widget         Attribute Selection
     ============== ===========================================================
-    User Readback  The widget first searches for a ``Signal`` called
-                   ``user_readback``. If this is not found the
-                   **first** hint is used.
+    User Readback  The ``readback_attribute`` property is used, which defaults
+                   to ``user_readback``.
 
-    Limit Switches Attributes ``low_limit_switch`` and ``high_limit_switch``
-                   are queried. If these are not found the widgets are hidden.
+    Limit Switches The ``low_limit_switch_attribute`` and
+                   ``high_limit_switch_attribute`` properties are used, which
+                   default to ``low_limit_switch`` and ``high_limit_switch``,
+                   respectively.
 
-    Soft Limits    The widget first looks for ``low_limit`` and ``high_limit``
-                   signals. If these do not exist, we look for ``limits`` and
-                   sets the text to match the returned tuple from this method.
+    Soft Limits    The ``low_limit_attribute`` and ``high_limit_attribute``
+                   properties are used, which default to ``low_limit`` and
+                   ``high_limit``, respectively.  As a fallback, the ``limit``
+                   property on the device may be queried directly.
 
     Set and Tweak  Both of these methods simply use ``Device.set`` which is
                    expected to take a ``float`` and return a ``status`` object
@@ -50,6 +100,11 @@ class TyphosPositionerWidget(TyphosBase, TyphosDesignerMixin):
     ============== ===========================================================
     """
     ui_template = os.path.join(ui_dir, 'positioner.ui')
+    _readback_attr = 'user_readback'
+    _low_limit_switch_attr = 'low_limit_switch'
+    _high_limit_switch_attr = 'high_limit_switch'
+    _low_limit_attr = 'low_limit'
+    _high_limit_attr = 'high_limit'
     _min_visible_operation = 0.1
 
     def __init__(self, parent=None):
@@ -135,55 +190,70 @@ class TyphosPositionerWidget(TyphosBase, TyphosDesignerMixin):
             raise Exception("No Device configured for widget!")
         return self._readback.get()
 
+    @_linked_attribute('_readback_attr', 'ui.user_readback')
+    def _link_readback(self, signal, widget):
+        """Link the positioner readback with the ui element."""
+        self._readback = signal
+
+    @_linked_attribute('_low_limit_switch_attr', 'ui.low_limit_switch')
+    def _link_low_limit_switch(self, signal, widget):
+        """Link the positioner lower limit switch with the ui element."""
+        if signal is None:
+            widget.hide()
+
+    @_linked_attribute('_high_limit_switch_attr', 'ui.high_limit_switch')
+    def _link_high_limit_switch(self, signal, widget):
+        """Link the positioner high limit switch with the ui element."""
+        if signal is None:
+            widget.hide()
+
+    @_linked_attribute('_low_limit_attr', 'ui.low_limit')
+    def _link_low_limit(self, signal, widget):
+        """Link the positioner lower limit with the ui element."""
+        return signal is not None
+
+    @_linked_attribute('_high_limit_attr', 'ui.high_limit')
+    def _link_high_limit(self, signal, widget):
+        """Link the positioner high limit with the ui element."""
+        return signal is not None
+
+    def _link_limits_by_limits_attr(self):
+        """Link limits by using ``device.limits``."""
+        device = self.device
+        try:
+            low_limit, high_limit = device.limits
+        except AttributeError:
+            ...
+        else:
+            if low_limit < high_limit:
+                self.ui.low_limit.setText(str(low_limit))
+                self.ui.high_limit.setText(str(high_limit))
+                return
+
+        # If not found or invalid, hide them:
+        self.ui.low_limit.hide()
+        self.ui.high_limit.hide()
+
+    @property
+    def device(self):
+        """The associated device."""
+        try:
+            return self.devices[0]
+        except Exception:
+            ...
+
     def add_device(self, device):
         """Add a device to the widget"""
         # Add device to cache
         self.devices.clear()  # only one device allowed
         super().add_device(device)
-        # Limit switches
-        for limit_switch in ('low_limit_switch',
-                             'high_limit_switch'):
-            # If our device has a limit switch attach it
-            if getattr(device, limit_switch, False):
-                widget = getattr(self.ui, limit_switch)
-                limit = getattr(device, limit_switch)
-                limit_chan = channel_from_signal(limit)
-                register_signal(limit)
-                widget.channel = limit_chan
-            # Otherwise, hide it the widget
-            else:
-                getattr(self.ui, limit_switch).hide()
-        # User Readback
-        if hasattr(device, 'user_readback'):
-            self._readback = device.user_readback
-        else:
-            # Let us assume it is the first hint
-            hinted = list(grab_kind(device, 'hinted').values())[0]
-            self._readback = hinted.signal
-        register_signal(self._readback)
-        self.ui.user_readback.channel = channel_from_signal(self._readback)
-        # Limit values
-        # Look for limit signals first
-        if isinstance(device, Device):
-            limit_signals = ('low_limit' in device.component_names
-                             and 'high_limit' in device.component_names)
-            # Use raw signals to keep widget updated
-            if limit_signals:
-                register_signal(device.low_limit)
-                low_lim_chan = channel_from_signal(device.low_limit)
-                self.ui.low_limit.channel = low_lim_chan
-                register_signal(device.high_limit)
-                high_lim_chan = channel_from_signal(device.high_limit)
-                self.ui.high_limit.channel = high_lim_chan
-                return
-        # Check that we have limits at all, or if they are implemented
-        if hasattr(device, 'limits') and device.limits[0] < device.limits[1]:
-            self.ui.low_limit.setText(str(device.limits[0]))
-            self.ui.high_limit.setText(str(device.limits[1]))
-        # Look for limit value components
-        else:
-            self.ui.low_limit.hide()
-            self.ui.high_limit.hide()
+
+        self._link_readback()
+        self._link_low_limit_switch()
+        self._link_high_limit_switch()
+
+        if not (self._link_low_limit() and self._link_high_limit()):
+            self._link_limits_by_limits_attr()
 
     @Property(bool, designable=False)
     def moving(self):
@@ -210,6 +280,51 @@ class TyphosPositionerWidget(TyphosBase, TyphosDesignerMixin):
     def failed_move(self):
         """The last requested move failed"""
         return self._last_move is False
+
+    @Property(str, designable=True)
+    def readback_attribute(self):
+        """The attribute name for the readback signal."""
+        return self._readback_attr
+
+    @readback_attribute.setter
+    def readback_attribute(self, value):
+        self._readback_attr = value
+
+    @Property(str, designable=True)
+    def low_limit_switch_attribute(self):
+        """The attribute name for the low limit switch signal."""
+        return self._low_limit_switch_attr
+
+    @low_limit_switch_attribute.setter
+    def low_limit_switch_attribute(self, value):
+        self._low_limit_switch_attr = value
+
+    @Property(str, designable=True)
+    def high_limit_switch_attribute(self):
+        """The attribute name for the high limit switch signal."""
+        return self._high_limit_switch_attr
+
+    @high_limit_switch_attribute.setter
+    def high_limit_switch_attribute(self, value):
+        self._high_limit_switch_attr = value
+
+    @Property(str, designable=True)
+    def low_limit_attribute(self):
+        """The attribute name for the low limit signal."""
+        return self._low_limit_attr
+
+    @low_limit_attribute.setter
+    def low_limit_attribute(self, value):
+        self._low_limit_attr = value
+
+    @Property(str, designable=True)
+    def high_limit_attribute(self):
+        """The attribute name for the high limit signal."""
+        return self._high_limit_attr
+
+    @high_limit_attribute.setter
+    def high_limit_attribute(self, value):
+        self._high_limit_attr = value
 
     def move_changed(self):
         """Called when a move is begun"""
