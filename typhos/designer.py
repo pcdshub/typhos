04160df91bd0b9b9f142477c7d50cef94390625e
K Lauer <klauer@slac.stanford.edu>
2020-05-15 09:29:10 -0700

    FIX: add missing composite signal panel plugin for designer

--- typhos/designer.py
+++ typhos/designer.py
@@ -5,7 +5,7 @@ from pydm.widgets.qtplugin_base import qtplugin_factory
 from .display import (TyphosDeviceDisplay, TyphosDisplaySwitcher,
                       TyphosDisplayTitle)
 from .func import TyphosMethodButton
-from .panel import TyphosSignalPanel
+from .panel import TyphosCompositeSignalPanel, TyphosSignalPanel
 from .positioner import TyphosPositionerWidget
 
 logger = logging.getLogger(__name__)
@@ -14,13 +14,15 @@ logger.info("Loading Typhos QtDesigner plugins ...")
 group_name = 'Typhos Widgets'
 TyphosSignalPanelPlugin = qtplugin_factory(TyphosSignalPanel,
                                            group=group_name)
+TyphosCompositeSignalPanelPlugin = qtplugin_factory(TyphosCompositeSignalPanel,
+                                                    group=group_name)
 TyphosDeviceDisplayPlugin = qtplugin_factory(TyphosDeviceDisplay,
                                              group=group_name)
 TyphosMethodButtonPlugin = qtplugin_factory(TyphosMethodButton,
                                             group=group_name)
 TyphosPositionerWidgetPlugin = qtplugin_factory(TyphosPositionerWidget,
                                                 group=group_name)
-TyphosDisplaySwitcher = qtplugin_factory(TyphosDisplaySwitcher,
-                                         group=group_name)
+TyphosDisplaySwitcherPlugin = qtplugin_factory(TyphosDisplaySwitcher,
+                                               group=group_name)
 TyphosDisplayTitlePlugin = qtplugin_factory(TyphosDisplayTitle,
                                             group=group_name)
