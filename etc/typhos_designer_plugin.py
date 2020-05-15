617f1447032d5abce0bb4beddc3558cf58302c59
K Lauer <klauer@slac.stanford.edu>
2020-05-15 10:47:19 -0700

    MNT: warn if PYDM_DESIGNER_ONLINE not set

--- etc/typhos_designer_plugin.py
+++ etc/typhos_designer_plugin.py
@@ -4,3 +4,9 @@ from typhos.designer import *  # noqa
 
 print("Loaded Typhos QtDesigner plugins. Available PyDM data plugins:",
       ', '.join(pydm.data_plugins.plugin_modules))
+
+if not pydm.config.DESIGNER_ONLINE:
+    print()
+    print("**WARNING**: PYDM_DESIGNER_ONLINE not set: no connections will be "
+          "made from widgets to the underlying control system.")
+    print()
