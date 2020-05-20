877436159f303e19449b309256461be7abe06ec6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-20 16:04:40 +0000

    Update docs after building Travis build 720 of pcdshub/typhos
    The docs were built from the branch 'master' against the commit
    f519b4405534ac9f3ac55935ea082ff270038277.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/typhos/jobs/689310119.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/typhos-env/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0+1.g28afb90',
+    VERSION: '1.0.0+3.gf519b44',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
