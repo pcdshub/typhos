6c5a96d92d01398a64621af5a9e0fec9fe512aa7
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-06-01 22:12:19 -0700

    BLD: Update conda-recipe to match conda-forge and also to be noarch.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,7 +1,7 @@
 # Install the package
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
-# Create auxillary
+# Create auxiliary dirs
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
 mkdir -p $PREFIX/etc/typhos
@@ -9,13 +9,20 @@ mkdir -p $PREFIX/etc/typhos
 # Create auxiliary vars
 DESIGNER_PLUGIN_PATH=$PREFIX/etc/typhos
 DESIGNER_PLUGIN=$DESIGNER_PLUGIN_PATH/typhos_designer_plugin.py
-ACTIVATE=$PREFIX/etc/conda/activate.d/typhos.sh
-DEACTIVATE=$PREFIX/etc/conda/deactivate.d/typhos.sh
+ACTIVATE=$PREFIX/etc/conda/activate.d/typhos
+DEACTIVATE=$PREFIX/etc/conda/deactivate.d/typhos
 
 echo "from typhos.designer import *" >> $DESIGNER_PLUGIN
-echo "export PYQTDESIGNERPATH="$DESIGNER_PLUGIN_PATH":\$PYQTDESIGNERPATH" >> $ACTIVATE
-echo "export PYDM_DESIGNER_ONLINE=True" >> $ACTIVATE
-echo "unset PYQTDESIGNERPATH" >> $DEACTIVATE
+
+echo "export PYQTDESIGNERPATH=\$CONDA_PREFIX/etc/typhos:\$PYQTDESIGNERPATH" >> $ACTIVATE.sh
+echo "unset PYQTDESIGNERPATH" >> $DEACTIVATE.sh
+
+echo '@echo OFF' >> $ACTIVATE.bat
+echo 'IF "%PYQTDESIGNERPATH%" == "" (' >> $ACTIVATE.bat
+echo 'set PYQTDESIGNERPATH=%CONDA_PREFIX%\etc\typhos' >> $ACTIVATE.bat
+echo ')ELSE (' >> $ACTIVATE.bat
+echo 'set PYQTDESIGNERPATH=%CONDA_PREFIX%\etc\typhos;%PYQTDESIGNERPATH%' >> $ACTIVATE.bat
+echo ')' >> $ACTIVATE.bat
 
 unset DESIGNER_PLUGIN_PATH
 unset DESIGNER_PLUGIN
