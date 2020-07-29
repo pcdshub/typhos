e2cce594d41968cb1c89433484a4578da12c5300
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-07-29 10:28:14 -0700

    DOC: Add doctr_versions_menu extension to properly render version menu. BLD: Add doctr-versions-menu to dev-requirements.txt.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting',
+    'doctr_versions_menu',
     'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
