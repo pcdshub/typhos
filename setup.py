481bdbf9a29c5b85351f1cf3ee130da7ec0ef8d1
Ken Lauer <klauer@slac.stanford.edu>
2022-05-13 10:11:00 -0700

    WIP: trying pydm designer widget entrypoints

--- setup.py
+++ setup.py
@@ -12,13 +12,18 @@ if len(git_requirements) > 0:
           "\n".join(f' {r}' for r in git_requirements))
 
 setup(
-    name='typhos',
+    name="typhos",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author='SLAC National Accelerator Laboratory',
+    author="SLAC National Accelerator Laboratory",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    description='Interface generation for ophyd devices',
-    entry_points={'console_scripts': ['typhos=typhos.cli:main']},
+    description="Interface generation for ophyd devices",
+    entry_points={
+        "console_scripts": ["typhos=typhos.cli:main"],
+        "pydm.widget": {
+            "TyphosSignalPanelPlugin=typhos.panel:TyphosSignalPanel",
+        }
+    },
 )
--- typhos/designer.py
+++ typhos/designer.py
@@ -10,30 +10,30 @@ from .positioner import TyphosPositionerWidget
 from .related_display import TyphosRelatedSuiteButton
 
 group_name = 'Typhos Widgets'
-TyphosSignalPanelPlugin = qtplugin_factory(TyphosSignalPanel,
-                                           group=group_name)
-TyphosCompositeSignalPanelPlugin = qtplugin_factory(TyphosCompositeSignalPanel,
-                                                    group=group_name)
-TyphosDeviceDisplayPlugin = qtplugin_factory(TyphosDeviceDisplay,
-                                             group=group_name)
-TyphosMethodButtonPlugin = qtplugin_factory(TyphosMethodButton,
-                                            group=group_name)
-TyphosPositionerWidgetPlugin = qtplugin_factory(TyphosPositionerWidget,
-                                                group=group_name)
-TyphosDisplaySwitcherPlugin = qtplugin_factory(TyphosDisplaySwitcher,
-                                               group=group_name)
-TyphosDisplayTitlePlugin = qtplugin_factory(TyphosDisplayTitle,
-                                            group=group_name)
-TyphosHelpFramePlugin = qtplugin_factory(TyphosHelpFrame, group=group_name)
-TyphosAlarmCirclePlugin = qtplugin_factory(TyphosAlarmCircle,
-                                           group=group_name)
-TyphosAlarmRectanglePlugin = qtplugin_factory(TyphosAlarmRectangle,
-                                              group=group_name)
-TyphosAlarmTrianglePlugin = qtplugin_factory(TyphosAlarmTriangle,
-                                             group=group_name)
-TyphosAlarmEllipsePlugin = qtplugin_factory(TyphosAlarmEllipse,
-                                            group=group_name)
-TyphosAlarmPolygonPlugin = qtplugin_factory(TyphosAlarmPolygon,
-                                            group=group_name)
-TyphosRelatedSuiteButtonPlugin = qtplugin_factory(TyphosRelatedSuiteButton,
-                                                  group=group_name)
+# TyphosSignalPanelPlugin = qtplugin_factory(TyphosSignalPanel,
+#                                            group=group_name)
+# TyphosCompositeSignalPanelPlugin = qtplugin_factory(TyphosCompositeSignalPanel,
+#                                                     group=group_name)
+# TyphosDeviceDisplayPlugin = qtplugin_factory(TyphosDeviceDisplay,
+#                                              group=group_name)
+# TyphosMethodButtonPlugin = qtplugin_factory(TyphosMethodButton,
+#                                             group=group_name)
+# TyphosPositionerWidgetPlugin = qtplugin_factory(TyphosPositionerWidget,
+#                                                 group=group_name)
+# TyphosDisplaySwitcherPlugin = qtplugin_factory(TyphosDisplaySwitcher,
+#                                                group=group_name)
+# TyphosDisplayTitlePlugin = qtplugin_factory(TyphosDisplayTitle,
+#                                             group=group_name)
+# TyphosHelpFramePlugin = qtplugin_factory(TyphosHelpFrame, group=group_name)
+# TyphosAlarmCirclePlugin = qtplugin_factory(TyphosAlarmCircle,
+#                                            group=group_name)
+# TyphosAlarmRectanglePlugin = qtplugin_factory(TyphosAlarmRectangle,
+#                                               group=group_name)
+# TyphosAlarmTrianglePlugin = qtplugin_factory(TyphosAlarmTriangle,
+#                                              group=group_name)
+# TyphosAlarmEllipsePlugin = qtplugin_factory(TyphosAlarmEllipse,
+#                                             group=group_name)
+# TyphosAlarmPolygonPlugin = qtplugin_factory(TyphosAlarmPolygon,
+#                                             group=group_name)
+# TyphosRelatedSuiteButtonPlugin = qtplugin_factory(TyphosRelatedSuiteButton,
+#                                                   group=group_name)
