68bb95a55620f0b2f14841b52b60c27a8df6080a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2022-10-20 18:19:47 +0000

    Update docs after building Travis build 1113 of pcdshub/typhos
    The docs were built from the tag 'master' against the commit
    c066e890b2f48d22ef629789fd706cc2c7dc2c87.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/pcdshub/typhos/jobs/586253097.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.13/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.2+29.g4bc5473',
-    LANGUAGE: 'None',
+    VERSION: '2.3.2+39.gc066e89',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
