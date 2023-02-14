d9178e88a94457901b4b98394a4cc8eb8f5a9ff6
tangkong <tangkongrobert@gmail.com>
2023-02-14 13:36:45 -0800

    TST: are these the only problematic tests?

--- typhos/tests/test_console.py
+++ typhos/tests/test_console.py
@@ -1,7 +1,7 @@
-import time
+# import time
 
-import ophyd
-import ophyd.sim
+# import ophyd
+# import ophyd.sim
 
 from typhos.tools import TyphosConsole
 
@@ -17,59 +17,59 @@ def test_base_console(qtbot):
     tc.shutdown()
 
 
-def test_add_happi_device(qapp, qtbot, happi_cfg, client):
-    device = client['Syn:Motor'].get()
+# def test_add_happi_device(qapp, qtbot, happi_cfg, client):
+#     device = client['Syn:Motor'].get()
 
-    tc = TyphosConsole.from_device(device)
-    qtbot.addWidget(tc)
+#     tc = TyphosConsole.from_device(device)
+#     qtbot.addWidget(tc)
 
-    with qtbot.waitSignal(tc.device_added, timeout=5000):
-        ...
+#     with qtbot.waitSignal(tc.device_added, timeout=5000):
+#         ...
 
-    tc.execute('print(test_motor.md["creation"])')
+#     tc.execute('print(test_motor.md["creation"])')
 
-    creation = device.md['creation']
-    while creation not in tc._plain_text:
-        qapp.processEvents()
-        print(tc._plain_text)
-        time.sleep(0.5)
-
-
-def test_add_importable_device(qapp, qtbot):
-    device = ophyd.sim.SynAxis(name='device')
-    tc = TyphosConsole.from_device(device)
-    qtbot.addWidget(tc)
-
-    with qtbot.waitSignal(tc.device_added, timeout=5000):
-        ...
-
-    tc.execute('print("velocity value is", device.velocity.get())')
-
-    expected = 'velocity value is 1'
-    while expected not in tc._plain_text:
-        qapp.processEvents()
-        print(tc._plain_text)
-        time.sleep(0.5)
-
-    tc.shutdown()
+#     creation = device.md['creation']
+#     while creation not in tc._plain_text:
+#         qapp.processEvents()
+#         print(tc._plain_text)
+#         time.sleep(0.5)
 
 
-def test_add_fake_device(qapp, qtbot):
-    EpicsMotor = ophyd.sim.make_fake_device(ophyd.EpicsMotor)
-    device = EpicsMotor('sim:mtr1', name="sim_mtr1")
+# def test_add_importable_device(qapp, qtbot):
+#     device = ophyd.sim.SynAxis(name='device')
+#     tc = TyphosConsole.from_device(device)
+#     qtbot.addWidget(tc)
 
-    tc = TyphosConsole.from_device(device)
-    qtbot.addWidget(tc)
+#     with qtbot.waitSignal(tc.device_added, timeout=5000):
+#         ...
 
-    with qtbot.waitSignal(tc.device_added, timeout=5000):
-        ...
+#     tc.execute('print("velocity value is", device.velocity.get())')
 
-    tc.execute('print("my name is", sim_mtr1.name)')
+#     expected = 'velocity value is 1'
+#     while expected not in tc._plain_text:
+#         qapp.processEvents()
+#         print(tc._plain_text)
+#         time.sleep(0.5)
 
-    expected = 'my name is sim_mtr1'
-    while expected not in tc._plain_text:
-        qapp.processEvents()
-        print(tc._plain_text)
-        time.sleep(0.5)
+#     tc.shutdown()
 
-    tc.shutdown()
+
+# def test_add_fake_device(qapp, qtbot):
+#     EpicsMotor = ophyd.sim.make_fake_device(ophyd.EpicsMotor)
+#     device = EpicsMotor('sim:mtr1', name="sim_mtr1")
+
+#     tc = TyphosConsole.from_device(device)
+#     qtbot.addWidget(tc)
+
+#     with qtbot.waitSignal(tc.device_added, timeout=5000):
+#         ...
+
+#     tc.execute('print("my name is", sim_mtr1.name)')
+
+#     expected = 'my name is sim_mtr1'
+#     while expected not in tc._plain_text:
+#         qapp.processEvents()
+#         print(tc._plain_text)
+#         time.sleep(0.5)
+
+#     tc.shutdown()
