f0cd1ab4af536a78cb094a4e273719e2f4d2bc09
Ken Lauer <klauer@slac.stanford.edu>
2023-08-28 11:52:34 -0700

    TST: fix test suite after switch to relative imports

---
+++ typhos/tests/empty_saved_suite.py
@@ -0,0 +1,4 @@
+# Note: this file is used in test_utils
+
+# Typhos expects to have saved something in this file which it will not find
+# This causes ``test_load_suite_with_bad_py_file`` to fail with ``AttributeError``
