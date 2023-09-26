109138ba29037d37334a2ddf22412a4f50fbeca1
Zachary Lentz <zlentz@slac.stanford.edu>
2023-09-26 15:13:06 -0700

    TST: add tests for precision defaults

--- typhos/tests/plugins/test_core.py
+++ typhos/tests/plugins/test_core.py
@@ -1,8 +1,13 @@
+from __future__ import annotations
+
 import numpy as np
 import pydm.utilities
+import pytest
 from ophyd import Component as Cpt
 from ophyd import Device, Signal
+from pydm import PyDMApplication
 from pydm.widgets import PyDMLineEdit
+from pytestqt.qtbot import QtBot
 
 from typhos.plugins.core import (SignalConnection, register_signal,
                                  signal_registry)
@@ -78,6 +83,53 @@ def test_metadata(qapp, qtbot):
     assert widget.enum_strings == ('a', 'b', 'c')
     assert widget._unit == 'urad'
     assert widget._prec == 2
+
+
+@pytest.mark.parametrize(
+    "sig_name,value,prec,expected",
+    [
+        ("no_prec_signal_float", 1.5, None, 3),
+        ("prec_signal_float", np.float32(2.718), 4, 4),
+        ("prec_signal_np_float", np.float32(3.14), 5, 5),
+        ("no_prec_signal_int", 1, None, 0),
+        ("prec_signal_int", 2, 2, 2),
+        ("zero_prec_float", 1.618, 0, 3),
+        ("neg_prec_float", 4.5, -2, 3),
+        ("neg_prec_int", 5, -30, 0),
+    ],
+)
+def test_precision_defaults_at_startup(
+    sig_name: str,
+    value: int | float | np.floating,
+    prec: int | None,
+    expected: int,
+    qapp: PyDMApplication,
+    qtbot: QtBot,
+):
+    """
+    Expected behavior:
+
+    - If a precision is zero, negative, or missing, use the
+      default precision of 3 for floats, 0 for ints.
+    - If a precision is valid and given, use the given precision.
+    """
+    widget = PyDMLineEdit()
+    qtbot.addWidget(widget)
+    widget.channel = f"sig://{sig_name}"
+    listener = widget.channels()[0]
+    # Create a signal and attach our listener
+    if prec is None:
+        sig = Signal(name=sig_name, value=value)
+    else:
+        sig = RichSignal(
+            name=sig_name,
+            value=value,
+            metadata={"precision": prec},
+        )
+    register_signal(sig)
+    _ = SignalConnection(listener, sig_name)
+    qapp.processEvents()
+    assert widget._prec == expected
 
 
 def test_disconnection(qtbot):
