9d3cff7fd937104a532641cba6ea87cabaa95450
tangkong <tangkongrobert@gmail.com>
2025-11-26 15:44:05 -0800

    split positioner

--- typhos/tests/test_positioner.py
+++ typhos/tests/test_positioner.py
@@ -146,158 +146,158 @@ def test_positioner_widget_set(motor_widget):
     assert motor.position == 4
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_positive_tweak(motor_widget):
-    motor, widget = motor_widget
-    widget.ui.tweak_value.setText('1')
-    widget.positive_tweak()
-    assert widget.ui.set_value.text() == '1.0'
-    assert motor.position == 1
+# @pytest.mark.no_gc
+# def test_positioner_widget_positive_tweak(motor_widget):
+#     motor, widget = motor_widget
+#     widget.ui.tweak_value.setText('1')
+#     widget.positive_tweak()
+#     assert widget.ui.set_value.text() == '1.0'
+#     assert motor.position == 1
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_negative_tweak(motor_widget):
-    motor, widget = motor_widget
-    widget.ui.tweak_value.setText('1')
-    widget.negative_tweak()
-    assert widget.ui.set_value.text() == '-1.0'
-    assert motor.position == -1
+# @pytest.mark.no_gc
+# def test_positioner_widget_negative_tweak(motor_widget):
+#     motor, widget = motor_widget
+#     widget.ui.tweak_value.setText('1')
+#     widget.negative_tweak()
+#     assert widget.ui.set_value.text() == '-1.0'
+#     assert motor.position == -1
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_moving_property(motor_widget, qtbot):
-    motor, widget = motor_widget
-    assert not widget.moving
-    motor.delay = 1.
-    widget.ui.set_value.setText('7')
-    widget.set()
-    qtbot.waitUntil(lambda: widget.moving, timeout=500)
-    qtbot.waitUntil(lambda: not widget.moving, timeout=1000)
+# @pytest.mark.no_gc
+# def test_positioner_widget_moving_property(motor_widget, qtbot):
+#     motor, widget = motor_widget
+#     assert not widget.moving
+#     motor.delay = 1.
+#     widget.ui.set_value.setText('7')
+#     widget.set()
+#     qtbot.waitUntil(lambda: widget.moving, timeout=500)
+#     qtbot.waitUntil(lambda: not widget.moving, timeout=1000)
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_last_move(motor_widget):
-    motor, widget = motor_widget
-    assert not widget.successful_move
-    assert not widget.failed_move
-    widget._status_finished(TyphosStatusResult.success)
-    assert widget.successful_move
-    assert not widget.failed_move
-    widget._status_finished(Exception())
-    assert not widget.successful_move
-    assert widget.failed_move
+# @pytest.mark.no_gc
+# def test_positioner_widget_last_move(motor_widget):
+#     motor, widget = motor_widget
+#     assert not widget.successful_move
+#     assert not widget.failed_move
+#     widget._status_finished(TyphosStatusResult.success)
+#     assert widget.successful_move
+#     assert not widget.failed_move
+#     widget._status_finished(Exception())
+#     assert not widget.successful_move
+#     assert widget.failed_move
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_moving_text_changes(motor_widget, qtbot):
-    motor, widget = motor_widget
+# @pytest.mark.no_gc
+# def test_positioner_widget_moving_text_changes(motor_widget, qtbot):
+#     motor, widget = motor_widget
 
-    def get_moving_text():
-        return widget.ui.moving_indicator_label.text()
+#     def get_moving_text():
+#         return widget.ui.moving_indicator_label.text()
 
-    start_text = get_moving_text()
-    motor.motor_is_moving.put(1)
-    qtbot.waitUntil(lambda: widget.moving, timeout=500)
-    move_text = get_moving_text()
-    motor.motor_is_moving.put(0)
-    qtbot.waitUntil(lambda: not widget.moving, timeout=500)
-    end_text = get_moving_text()
-    assert start_text != move_text
-    assert move_text != end_text
-    assert end_text == start_text
-
-
-@pytest.mark.no_gc
-def test_positioner_widget_alarm_text_changes(motor_widget, qtbot):
-    motor, widget = motor_widget
-    alarm_texts = []
-
-    def get_alarm_text():
-        return widget.ui.alarm_label.text()
-
-    def update_alarm(level, connected=True):
-        with qtbot.waitSignal(
-            widget.ui.alarm_circle.alarm_changed,
-            timeout=500,
-        ):
-            motor.motor_is_moving.update_metadata(
-                {
-                    'severity': level,
-                    'connected': connected,
-                }
-            )
-
-    def check_alarm_text_at_level(level):
-        update_alarm(level)
-        alarm_texts.append(get_alarm_text())
-
-    update_alarm(0, connected=False)
-
-    for num in range(4):
-        check_alarm_text_at_level(num)
-
-    for text in alarm_texts:
-        assert alarm_texts.count(text) == 1
+#     start_text = get_moving_text()
+#     motor.motor_is_moving.put(1)
+#     qtbot.waitUntil(lambda: widget.moving, timeout=500)
+#     move_text = get_moving_text()
+#     motor.motor_is_moving.put(0)
+#     qtbot.waitUntil(lambda: not widget.moving, timeout=500)
+#     end_text = get_moving_text()
+#     assert start_text != move_text
+#     assert move_text != end_text
+#     assert end_text == start_text
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_alarm_kind_level(motor_widget, qtbot):
-    motor, widget = motor_widget
-    # Alarm widget has its own tests
-    # Just make sure the levels match when set
-    assert widget.alarmKindLevel == widget.ui.alarm_circle.kindLevel
-    for kind_level in KindLevel:
-        widget.alarmKindLevel = kind_level
-        assert widget.alarmKindLevel == kind_level
-        assert widget.ui.alarm_circle.kindLevel == kind_level
+# @pytest.mark.no_gc
+# def test_positioner_widget_alarm_text_changes(motor_widget, qtbot):
+#     motor, widget = motor_widget
+#     alarm_texts = []
+
+#     def get_alarm_text():
+#         return widget.ui.alarm_label.text()
+
+#     def update_alarm(level, connected=True):
+#         with qtbot.waitSignal(
+#             widget.ui.alarm_circle.alarm_changed,
+#             timeout=500,
+#         ):
+#             motor.motor_is_moving.update_metadata(
+#                 {
+#                     'severity': level,
+#                     'connected': connected,
+#                 }
+#             )
+
+#     def check_alarm_text_at_level(level):
+#         update_alarm(level)
+#         alarm_texts.append(get_alarm_text())
+
+#     update_alarm(0, connected=False)
+
+#     for num in range(4):
+#         check_alarm_text_at_level(num)
+
+#     for text in alarm_texts:
+#         assert alarm_texts.count(text) == 1
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_clear_error(motor_widget, qtbot):
-    motor, widget = motor_widget
-    widget.clear_error()
-    qtbot.waitUntil(lambda: motor.clear_error.called, timeout=500)
+# @pytest.mark.no_gc
+# def test_positioner_widget_alarm_kind_level(motor_widget, qtbot):
+#     motor, widget = motor_widget
+#     # Alarm widget has its own tests
+#     # Just make sure the levels match when set
+#     assert widget.alarmKindLevel == widget.ui.alarm_circle.kindLevel
+#     for kind_level in KindLevel:
+#         widget.alarmKindLevel = kind_level
+#         assert widget.alarmKindLevel == kind_level
+#         assert widget.ui.alarm_circle.kindLevel == kind_level
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_move_error(motor_widget, qtbot):
-    motor, widget = motor_widget
-    bad_position = motor.high_limit.get() + 1
-
-    with pytest.raises(LimitError):
-        motor.check_value(bad_position)
-
-    assert widget.ui.status_label.text() == ''
-    widget._set(bad_position)
-
-    def has_limit_error():
-        assert 'LimitError' in widget.ui.status_label.text()
-
-    qtbot.waitUntil(has_limit_error, timeout=1000)
+# @pytest.mark.no_gc
+# def test_positioner_widget_clear_error(motor_widget, qtbot):
+#     motor, widget = motor_widget
+#     widget.clear_error()
+#     qtbot.waitUntil(lambda: motor.clear_error.called, timeout=500)
 
 
-@pytest.mark.no_gc
-def test_positioner_widget_long_status_text(motor_widget):
-    _, widget = motor_widget
+# @pytest.mark.no_gc
+# def test_positioner_widget_move_error(motor_widget, qtbot):
+#     motor, widget = motor_widget
+#     bad_position = motor.high_limit.get() + 1
 
-    assert widget.ui.status_label.text() == ''
+#     with pytest.raises(LimitError):
+#         motor.check_value(bad_position)
 
-    widget._set_status_text("Oh no! " * 1000, max_length=50)
+#     assert widget.ui.status_label.text() == ''
+#     widget._set(bad_position)
 
-    text = widget.ui.status_label.text()
-    assert text.endswith("...")
-    assert len(text) < 60
+#     def has_limit_error():
+#         assert 'LimitError' in widget.ui.status_label.text()
 
-    widget._set_status_text("")
-    assert widget.ui.status_label.text() == ''
+#     qtbot.waitUntil(has_limit_error, timeout=1000)
 
-    widget._set_status_text(
-        TyphosStatusMessage(
-            "Error! " * 1000,
-            tooltip="Test suite giant error string!",
-        ),
-        max_length=50,
-    )
-    text = widget.ui.status_label.text()
-    assert text.endswith("...")
-    assert len(text) < 60
+
+# @pytest.mark.no_gc
+# def test_positioner_widget_long_status_text(motor_widget):
+#     _, widget = motor_widget
+
+#     assert widget.ui.status_label.text() == ''
+
+#     widget._set_status_text("Oh no! " * 1000, max_length=50)
+
+#     text = widget.ui.status_label.text()
+#     assert text.endswith("...")
+#     assert len(text) < 60
+
+#     widget._set_status_text("")
+#     assert widget.ui.status_label.text() == ''
+
+#     widget._set_status_text(
+#         TyphosStatusMessage(
+#             "Error! " * 1000,
+#             tooltip="Test suite giant error string!",
+#         ),
+#         max_length=50,
+#     )
+#     text = widget.ui.status_label.text()
+#     assert text.endswith("...")
+#     assert len(text) < 60
